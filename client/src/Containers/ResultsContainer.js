7238ed245bfa24ce6c99b1895563d5e1c5fc60b1
terrell linnell <terrell.linnell@gmail.com>
2017-04-17 14:28:43 -0600

    got results from craigslis free section

--- client/src/Containers/ResultsContainer.js
+++ client/src/Containers/ResultsContainer.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
+import $ from 'jquery';
 import ResultsDisplay from '../Views/ResultsDisplay';
 import DetailsDisplay from '../Views/DetailsDisplay';
 
 class ResultsContainer extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      resultsInfo : null
+    }
+  }
+
+  componentWillMount = () => {this.scrapeInfo()}
+
+  scrapeInfo = () => {
+    $.ajax({
+      url: '/api/results',
+      method: 'GET'
+    }).done((data) => {
+      this.setState({resultsInfo: data})
+    })
+  }
   render () {
     return (
       <div>
-        {this.state.detailsId ? <DetailsDisplay /> : <ResultsDisplay />}
+         {this.state.resultsInfo !== null ? <ResultsDisplay resultsInfo={this.state.resultsInfo}/> : null}
+         {this.state.detailsId ? <DetailsDisplay /> : null}
       </div>
     )
   }
